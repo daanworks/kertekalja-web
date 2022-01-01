c7d4979863650854bfd37498fe25761e685f3499
daanworks <daniel.babinszky@gmail.com>
2022-01-01 21:15:07 +0100

    favicon

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import './App.css';
 import Header from "./components/Header";
 import Main from "./components/Main";
-import {useEffect} from "react";
 import Footer from "./components/Footer";
 import styled from "styled-components";
 
-
-
 function App() {
-
-  useEffect(() => {
-    document.title = 'Kertek Alja'
-    let link = document.querySelector("link[rel~='icon']");
-    if (!link) {
-      link = document.createElement('link');
-      link.rel = 'icon';
-      document.getElementsByTagName('head')[0].appendChild(link);
-    }
-    link.href = '/public/favicon-16x16.png?v=2';
-  }, [])
 
   return (
     <div className="App">
